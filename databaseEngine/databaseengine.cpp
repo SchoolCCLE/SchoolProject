b02a24cb6f6939e0bb84674671a6d1e584e9d058
Ruben Lopez <ruben.lopez@hpcds.com>
2014-11-23 01:29:50 +0100

    Update Users Class

--- databaseEngine/databaseengine.cpp
+++ databaseEngine/databaseengine.cpp
@@ -1,7 +1,7 @@
 #include "databaseengine.h"
 #include <assert.h>
 #include <QDebug>
-
+#include <QSqlQuery>
 
 Users *Users::getInstance()
 {
@@ -9,28 +9,100 @@ Users *Users::getInstance()
     return theInstance;
 }
 
-Users::Users(QObject *parent):QObject(parent)
+void Users::setDatabase(QSqlDatabase dataBase)
+{
+    dataBase_ = dataBase;
+    sqlQuery_ = new QSqlQuery(dataBase_);
+    qDebug () << "Created table users" << createTables();
+}
+
+bool Users::setUser(QList<QVariant> data)
+{
+    bool result = sqlQuery_->exec(QString("INSERT OR REPLACE INTO Users VALUES (%1,'%2','%3','%4')") \
+                           .arg(data.at(0).toInt()) \
+                           .arg(data.at(1).toString()) \
+                           .arg(data.at(2).toString()) \
+                           .arg(data.at(3).toString()));
+    if(result)
+    {
+        emit userDataChanged();
+    }
+    return result;
+}
+
+Users::Users(QObject *parent):QObject(parent),sqlQuery_(NULL)
 {
 
+}
+
+bool Users::createTables()
+{
+    assert( dataBase_.isOpen() );
+    assert(sqlQuery_);
+
+
+    bool result = true;
+
+    result |= sqlQuery_->exec("CREATE TABLE IF NOT EXISTS 'Users'"
+                              "('UserId'  INTEGER PRIMARY KEY,"
+                              "'UserName' TEXT NOT NULL  UNIQUE,"
+                              "'FullName' TEXT,"
+                              "'Passwd'   TEXT)");
+
+    QList<QVariant> data;
+    data << QVariant(1) << QVariant("rootben") << QVariant("Super User") << QVariant("PASS");
+
+    result |= setUser(data);
+
+
+
+    result |= sqlQuery_->exec("CREATE TABLE IF NOT EXISTS 'AccessLevel'"
+                              "('LevelId' INTEGER PRIMARY KEY,"
+                              "'LevelName' TEX T NOT NULL UNIQUE)");
+    result |= sqlQuery_->exec(QString("INSERT OR REPLACE INTO AccessLevel VALUES (1,'Admin')"));
+    result |= sqlQuery_->exec(QString("INSERT OR REPLACE INTO AccessLevel VALUES (2,'User')"));
+
+
+    result |= sqlQuery_->exec("CREATE TABLE IF NOT EXISTS 'UserAccess'"
+                              "('UserId' INTEGER ,"
+                              "'LevelId' INTEGER ,"
+                              "PRIMARY KEY ('UserId', 'LevelId'))");
+    result |= sqlQuery_->exec(QString("INSERT OR REPLACE INTO UserAccess VALUES (1,1)"));
+
+    return result;
 }
 
 DatabaseEngine *DatabaseEngine::getInstance()
 {
     static DatabaseEngine* theInsance = new DatabaseEngine();
-    static bool initialized = false;
-    if(!initialized){
-        initialized = true;
-        assert(theInsance->openDB());
-    }
 
     return theInsance;
+}
+
+DatabaseEngine::~DatabaseEngine()
+{
+    dataBase_.close();
+}
+
+bool DatabaseEngine::closeDB()
+{
+    dataBase_.close();
+    return !dataBase_.isOpen();
+
 }
 
 DatabaseEngine::DatabaseEngine(QObject *parent) :
     QObject(parent)
 {
+    static bool initialized = false;
+    if(!initialized){
+        initialized = true;
+        openDB();
+    }
     users_ = Users::getInstance();
     users_->setParent(this);
+    users_->setDatabase(dataBase_);
+
 }
 
 bool DatabaseEngine::openDB()
@@ -42,20 +114,21 @@ bool DatabaseEngine::openDB()
     QDir dir(path);
     dir.mkdir(path);
     path.append(QDir::separator()).append("school.db.sqlite");
-
-    qDebug() << "Path " << path;
+    databaseName_ = path;
+    qDebug() << "Path " << databaseName_;
     dataBase_.setDatabaseName(path);
     return dataBase_.open();
 }
 
 bool DatabaseEngine::deleteDB()
 {
-    return true;
+    dataBase_.close();
+    return QFile::remove(databaseName_);
 }
 
 QSqlError DatabaseEngine::lastError()
 {
-    return QSqlError();
+    return dataBase_.lastError();
 }
 
 
--- databaseEngine/databaseengine.h
+++ databaseEngine/databaseengine.h
@@ -9,19 +9,46 @@
 #include <QSqlError>
 #include <QFile>
 #include <QDir>
+#include <QString>
+
+typedef QMap<int, QList<QVariant> > datamap;
+
+
 
 /**
- * @brief The Users class provide information about the user present into application
+ * @brief The Users class provide information about the users
  */
 class Users : public QObject
 {
     Q_OBJECT
 public:
+
+    /**
+     * @brief getInstance. Instance of user
+     * @return pointer to user
+     */
     static Users* getInstance();
+
+    /**
+     * @brief setDatabase, Asign a database to user class where the data will be stored
+     * @param dataBase
+     */
+    void setDatabase(QSqlDatabase dataBase);
+
+    datamap getUsers();
+    bool setUser(QList<QVariant> data);
+
     virtual ~Users() {}
 private:
     Users(QObject* parent=0);
-    QMap<int, QList<QVariant> > userList_;
+    datamap userList_;
+    QSqlDatabase dataBase_;
+    QSqlQuery* sqlQuery_;
+
+    bool createTables();
+signals:
+    void userDataChanged();
+
 };
 
 
@@ -29,13 +56,18 @@ class DatabaseEngine : public QObject
 {
     Q_OBJECT
 public:
-    static DatabaseEngine * getInstance();
 
+
+    static DatabaseEngine * getInstance();
+    virtual ~DatabaseEngine();
+    bool closeDB();
 signals:
 
 public slots:
 private:
     explicit DatabaseEngine(QObject *parent = 0);
+    QString databaseName_;
+
     Users* users_;
     QSqlDatabase dataBase_;
 
