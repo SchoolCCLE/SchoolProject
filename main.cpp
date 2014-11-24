9e9d9f63fedbbfb40cd933f7ddb91a2064c4e727
Pablo <pablo@pabloCorp.es>
2014-11-24 12:51:17 +0100

    Añado esbozo main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include "databaseEngine/databaseengine.h"
+#include <QQmlEngine>
+#include <QQmlContext>
+
+
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    Users*_model = _model->getInstance();
+
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("_model",_model);
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
