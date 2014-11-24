1dc1ce6d53ecb9197045cb7969b0da449fca400e
zapi <zapisanchez@gmail.com>
2014-11-24 13:01:44 +0100

    todo

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,12 +1,30 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickView>
+#include <QQmlContext>
+#include <QQuickItem>
+//#include <controler.h>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    //QQmlApplicationEngine engine;
+    //engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    QQmlEngine *engine_= new QQmlEngine();
+    QQuickView *view = new QQuickView(engine_,NULL);
+
+    //Controler *obj = new Controler();
+//    view->rootContext()->setContextProperty("obj_",obj);
+
+    view->setSource(QUrl(QStringLiteral("qrc:/main.qml")));
+    view->show();
+
+    /*
+     *  Aquí van los connect
+     */
+
 
     return app.exec();
 }
