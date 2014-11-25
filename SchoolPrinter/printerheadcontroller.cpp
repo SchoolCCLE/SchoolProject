9535a8a632cfc1663af2babd554621815de46248
Pablo <pablo@pabloCorp.es>
2014-11-25 10:55:02 +0100

    PrinterHeadController added

---
+++ SchoolPrinter/printerheadcontroller.cpp
@@ -0,0 +1,21 @@
+#include "printerheadcontroller.h"
+
+printerHeadcontroller::printerHeadcontroller()
+{
+}
+
+int printerHeadcontroller::getID(){
+
+}
+
+int printerHeadcontroller::getSalud(){
+
+}
+
+int printerHeadcontroller::getWarranty(){
+
+}
+
+int printerHeadcontroller::getID(){
+
+}
---
+++ SchoolPrinter/printerheadcontroller.h
@@ -0,0 +1,10 @@
+#ifndef PRINTERHEADCONTROLLER_H
+#define PRINTERHEADCONTROLLER_H
+
+class printerHeadcontroller
+{
+public:
+    printerHeadcontroller();
+};
+
+#endif // PRINTERHEADCONTROLLER_H
