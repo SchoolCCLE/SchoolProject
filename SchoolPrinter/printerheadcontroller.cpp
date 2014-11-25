c62dcac5d9a898eaed4e47a0a60782cc56a28a3d
Pablo <pablo@pabloCorp.es>
2014-11-25 12:24:14 +0100

    More printer controller added

--- SchoolPrinter/printerheadcontroller.cpp
+++ SchoolPrinter/printerheadcontroller.cpp
@@ -1,21 +1,78 @@
 #include "printerheadcontroller.h"
 
-printerHeadcontroller::printerHeadcontroller()
+printerHeadcontroller *printerHeadcontroller::getInstance()
 {
+    static printerHeadcontroller * theInstance = new printerHeadcontroller();
+    return theInstance;
 }
 
-int printerHeadcontroller::getID(){
+printerHeadcontroller::printerHeadcontroller(){
+    dataBase_ = DatabaseEngine::getInstance();
+    dataBase_->setParent(this);
+    QList<QVariant> data;
+
+    data.clear();
+    data << QVariant(1) << QVariant("red")<<QVariant("95")<<QVariant(getWarranty(1))<<QVariant("12/04/2014");
+    dataBase_->setPrinthead(data);
+    data.clear;
+    data << QVariant(2) << QVariant("blue")<<QVariant("97")<<QVariant(getWarranty(2))<<QVariant("12/04/2014");
+    dataBase_->setPrinthead(data);
+    data.clear;
+    data << QVariant(3) << QVariant("brown")<<QVariant("85")<<QVariant(getWarranty(2))<<QVariant("12/04/2014");
+    dataBase_->setPrinthead(data);
+    data.clear;
+    data << QVariant(4) << QVariant("grey")<<QVariant("46")<<QVariant(getWarranty(2))<<QVariant("12/04/2014");
+    dataBase_->setPrinthead(data);
+    data.clear;
+    data << QVariant(5) << QVariant("green")<<QVariant("87")<<QVariant(getWarranty(2))<<QVariant("12/04/2014");
+    dataBase_->setPrinthead(data);
+    data.clear;
+    data << QVariant(6) << QVariant("orange")<<QVariant("95")<<QVariant(getWarranty(2))<<QVariant("12/04/2014");
+    dataBase_->setPrinthead(data);
+    data.clear;
+    data << QVariant(7) << QVariant("yellow")<<QVariant("91")<<QVariant(getWarranty(2))<<QVariant("12/04/2014");
+    dataBase_->setPrinthead(data);
+    data.clear;
+    data << QVariant(8) << QVariant("black")<<QVariant("49")<<QVariant(getWarranty(2))<<QVariant("12/04/2014");
+    dataBase_->setPrinthead(data);
+    data.clear;
 
 }
 
-int printerHeadcontroller::getSalud(){
+
+QString printerHeadcontroller::getColor(QString id){
+    datamap printerHeads = dataBase_->getPrintheads();
+    for (datamap::iterator it = printerHeads.begin() ; it != printerHeads.end(); ++it)
+    {
+        if(it.value().at(0).toString() == id)
+        {
+            return it.value().at(1).toString();
+        }
+    }
 
 }
 
-int printerHeadcontroller::getWarranty(){
+QString printerHeadcontroller::getSalud(QString id){
 
+    datamap printerHeads = dataBase_->getPrintheads();
+    for (datamap::iterator it = printerHeads.begin() ; it != printerHeads.end(); ++it)
+    {
+        if(it.value().at(0).toString() == id)
+        {
+            return it.value().at(2).toString();
+        }
+    }
 }
 
-int printerHeadcontroller::getID(){
+QString printerHeadcontroller::getWarranty(QList<QVariant> printHead){
 
+    datamap printerHeads = dataBase_->getPrintheads();
+    for (datamap::iterator it = printerHeads.begin() ; it != printerHeads.end(); ++it)
+    {
+        if(it.value().at(0).toString() == id)
+        {
+
+        }
+    }
 }
+
--- SchoolPrinter/printerheadcontroller.h
+++ SchoolPrinter/printerheadcontroller.h
@@ -1,10 +1,26 @@
 #ifndef PRINTERHEADCONTROLLER_H
 #define PRINTERHEADCONTROLLER_H
 
-class printerHeadcontroller
+#include <QObject>
+#include "../databaseEngine/databaseengine.h"
+
+
+class printerHeadcontroller: public QObject
 {
+    Q_OBJECT
 public:
-    printerHeadcontroller();
+    static printerHeadcontroller* getInstance();
+    QString getSalud(QString id);
+    QString getColor(QString id);
+    QString getWarranty(QList<QVariant> printHead);
+signals:
+    Q_INVOKABLE void statusFailed(bool status);
+public slots:
+    void checkStatus(QString);
+private:
+    explicit printerHeadcontroller(QObject *parent = 0);
+    DatabaseEngine* dataBase_;
+
 };
 
 #endif // PRINTERHEADCONTROLLER_H
