22abde9b42580d82b15f6503fb21a5832fb7e109
Alberto <albertobahillo@gmail.com>
2014-11-26 12:18:51 +0100

    Commit clases PrinterEngine y Cartridge

--- SchoolPrinter/cartridge.h
+++ SchoolPrinter/cartridge.h
@@ -2,17 +2,90 @@
 #define CARTRIDGE_H
 
 #include <QObject>
+#include <QString>
 
 class Cartridge : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(int level READ getLevel WRITE setLevel NOTIFY levelChanged)
+    Q_PROPERTY(int capacity READ getCapacity WRITE setCapacity NOTIFY capacityChanged)
+    Q_PROPERTY(QString color READ getColor WRITE setColor NOTIFY colorChanged)
+    Q_PROPERTY(int cartridgeStatus READ getCartridgeStatus WRITE setCartridgeStatus NOTIFY cartridgeStatusChanged)
+    int m_level;
+
+    int m_capacity;
+
+    QString m_color;
+
+    int m_cartridgeStatus;
+
 public:
-    explicit Cartridge(QObject *parent = 0);
+    explicit Cartridge(QString col,int level,int capacity, QObject *parent = 0);
+
+int getLevel() const
+{
+    return m_level;
+}
+
+int getCapacity() const
+{
+    return m_capacity;
+}
+
+QString getColor() const
+{
+    return m_color;
+}
+
+int getCartridgeStatus() const
+{
+    return m_cartridgeStatus;
+}
 
 signals:
 
+void levelChanged(int arg);
+
+void capacityChanged(int arg);
+
+void colorChanged(QString arg);
+
+void cartridgeStatusChanged(int arg);
+
 public slots:
 
+void setLevel(int arg)
+{
+    if (m_level == arg)
+        return;
+
+    m_level = arg;
+    emit levelChanged(arg);
+}
+void setCapacity(int arg)
+{
+    if (m_capacity == arg)
+        return;
+
+    m_capacity = arg;
+    emit capacityChanged(arg);
+}
+void setColor(QString arg)
+{
+    if (m_color == arg)
+        return;
+
+    m_color = arg;
+    emit colorChanged(arg);
+}
+void setCartridgeStatus(int arg)
+{
+    if (m_cartridgeStatus == arg)
+        return;
+
+    m_cartridgeStatus = arg;
+    emit cartridgeStatusChanged(arg);
+}
 };
 
 #endif // CARTRIDGE_H
--- SchoolPrinter/printerengine.cpp
+++ SchoolPrinter/printerengine.cpp
@@ -4,5 +4,39 @@ PrinterEngine::PrinterEngine(int type, QObject *parent) :
     QObject(parent)
 {
 
-    setPrinterType(type);
+    m_printerType = type;
+    if (m_printerType == 0){
+    m_cartridges.append(new Cartridge("Cyan"));
+    m_cartridges.append(new Cartridge("Magenta"));
+    m_cartridges.append(new Cartridge("Yellow"));
+    m_cartridges.append(new Cartridge("Black"));
+    }
+    if (m_printerType == 1){
+    m_cartridges.append(new Cartridge("Cyan"));
+    m_cartridges.append(new Cartridge("Magenta"));
+    m_cartridges.append(new Cartridge("Yellow"));
+    m_cartridges.append(new Cartridge("Black"));
+    m_cartridges.append(new Cartridge("Cyan"));
+    m_cartridges.append(new Cartridge("Magenta"));
+    m_cartridges.append(new Cartridge("Yellow"));
+    m_cartridges.append(new Cartridge("Black"));
+    }
+
+}
+
+void PrinterEngine::changeState(int state)
+{
+    if (state ==1){
+        m_stateNumber = 1;
+        m_stateText = "Idle";
+    }
+    if (state ==3){
+        m_stateNumber = 3;
+        m_stateText = "Warning";
+    }
+    if (state ==4){
+        m_stateNumber = 4;
+        m_stateText = "Error";
+    }
+
 }
